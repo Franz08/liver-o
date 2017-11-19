143c37419008ef271a0c0868b1d2245581477b9c
Franz08 <blicanel@yahoo.com>
2017-11-20 07:22:28 +0800

    File Transfer
    - seperated mock version of the assessment to test-assessment folder

---
+++ (maybe)joli-admin-master/Test-Assessment/js/question.js
@@ -0,0 +1,76 @@
+var questions = [{
+    "Question": "Do you suffer nausea?",
+    "option1": "Yes",
+    "option2": "No",
+    "option3": "I Don't Know"
+}, {
+    "Question": "Has any drug ever damaged your liver?",
+    "option1": "Yes",
+    "option2": "No",
+    "option3": "I Don't Know"
+}, {
+    "Question": "Do you have a lot of allergies?",
+    "option1": "Yes",
+    "option2": "No",
+    "option3": "I Don't Know"
+}, {
+    "Question": "Do you suffer excess body heat?",
+    "option1": "Yes",
+    "option2": "No",
+    "option3": "I Don't Know"
+}, {
+    "Question": "Have you ever been diagnosed with jaundice?",
+    "option1": "Yes",
+    "option2": "No",
+    "option3": "I Don't Know"
+}, {
+    "Question": "Have you ever been diagnosed with fatty liver?",
+    "option1": "Yes",
+    "option2": "No",
+    "option3": "I Don't Know"
+}, {
+    "Question": "Do you drink too much alcohol?",
+    "option1": "Yes",
+    "option2": "No",
+    "option3": "I Don't Know"
+}, {
+    "Question": "Are you intolerant to alcohol?",
+    "option1": "Yes",
+    "option2": "No",
+    "option3": "I Don't Know"
+}, {
+    "Question": "Do you have gallstones?",
+    "option1": "Yes",
+    "option2": "No",
+    "option3": "I Don't Know"
+}, {
+    "Question": "Do you take a lot of pain killers?",
+    "option1": "Yes",
+    "option2": "No",
+    "option3": "I Don't Know"
+}, {
+    "Question": "Have you taken a lot of antibiotic drugs?",
+    "option1": "Yes",
+    "option2": "No",
+    "option3": "I Don't Know"
+}, {
+    "Question": "Do you have pain/discomfort over the right upper abdominal area?",
+    "option1": "Yes",
+    "option2": "No",
+    "option3": "I Don't Know"
+}, {
+    "Question": "Do you get itchy skin?",
+    "option1": "Yes",
+    "option2": "No",
+    "option3": "I Don't Know"
+}, {
+    "Question": "Do you have lot of skin rashes?",
+    "option1": "Yes",
+    "option2": "No",
+    "option3": "I Don't Know"
+}, {
+    "Question": "Do you have brown liver spots on your skin?",
+    "option1": "Yes",
+    "option2": "No",
+    "option3": "I Don't Know"
+}]
